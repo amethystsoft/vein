c369c71118722432ecb4b3acd54caf8846a33700
Mia Koring <koring.mia@gmail.com>
2026-01-20 17:37:04 +0100

    moved more to use suffix instead of direct match

--- Sources/CommonVeinMacroLogic/ModelMacro.swift
+++ Sources/CommonVeinMacroLogic/ModelMacro.swift
@@ -22,7 +22,7 @@ public struct Model {
                 let hasFieldAttribute = varDecl.attributes.contains { attr in
                     if let attrSyntax = attr.as(AttributeSyntax.self),
                        let name = attrSyntax.attributeName.as(IdentifierTypeSyntax.self) {
-                        return name.name.text == "LazyField"
+                        return name.name.text.hasSuffix("LazyField")
                     }
                     return false
                 }
@@ -38,7 +38,7 @@ public struct Model {
                 let hasFieldAttribute = varDecl.attributes.contains { attr in
                     if let attrSyntax = attr.as(AttributeSyntax.self),
                        let name = attrSyntax.attributeName.as(IdentifierTypeSyntax.self) {
-                        return name.name.text == "Field"
+                        return name.name.text.hasSuffix("Field")
                     }
                     return false
                 }
