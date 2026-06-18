b13fe4aade9f0bb696ec4c307831ca70e58c48ba
Mia Koring <koring.mia@gmail.com>
2026-06-19 00:17:54 +0200

    made ulid test a conditional target

--- Package.swift
+++ Package.swift
@@ -21,6 +21,89 @@ var veinDependencies: [Target.Dependency] = [
     )
 ]
 
+var targets: [Target] = [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+        name: "Vein",
+        dependencies: veinDependencies
+    ),
+    .target(
+        name: "VeinCore",
+        dependencies: [
+            "Vein",
+            "VeinCoreMacros",
+        ]
+    ),
+    .target(
+        name: "VeinSwiftUI",
+        dependencies: [
+            "Vein",
+            "VeinSwiftUIMacros"
+        ]
+    ),
+    .target(
+        name: "VeinTesting",
+        dependencies: [
+            "Vein"
+        ]
+    ),
+    .macro(
+        name: "VeinCoreMacros",
+        dependencies: [
+            .product(name: "SwiftSyntax", package: "swift-syntax"),
+            .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+            .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            .product(name: "VeinMacrosCore", package: "VeinMacrosCore"),
+        ]
+    ),
+    .macro(
+        name: "VeinSwiftUIMacros",
+        dependencies: [
+            .product(name: "SwiftSyntax", package: "swift-syntax"),
+            .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+            .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            .product(name: "VeinMacrosCore", package: "VeinMacrosCore"),
+        ]
+    ),
+    .target(
+        name: "VeinMacrosBaseWrapper",
+        dependencies: [
+            .product(name: "VeinMacrosCore", package: "VeinMacrosCore")
+        ],
+    ),
+    .target(name: "ULID"),
+    .testTarget(
+        name: "VeinTests",
+        dependencies: [
+            "Vein",
+            "VeinCore",
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "SQLiteDB", package: "swift-sqlcipher"),
+            .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
+        ]
+    ),
+    .testTarget(
+        name: "VeinTestingTests",
+        dependencies: [
+            "VeinTesting",
+            "Vein",
+            "VeinCore",
+            "VeinCoreMacros",
+            .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+            .product(name: "Logging", package: "swift-log")
+        ]
+    ),
+]
+#if os(macOS)
+targets.append(
+    .testTarget(
+        name: "ULIDTests",
+        dependencies: ["ULID"]
+    )
+)
+#endif
 
 let package = Package(
     name: "amethyst-vein",
@@ -55,93 +138,15 @@ let package = Package(
         .package(path: "./VeinMacrosCore"),
         // SQLite >= 3.45.0 is required to support JSONB.
         // The bundled version of swift-sqlcipher >= 1.9.0 matches that requirement.
-        .package(
-            url: "https://github.com/skiptools/swift-sqlcipher",
-            .upToNextMajor(from: "1.9.0")
-        ),
+            .package(
+                url: "https://github.com/skiptools/swift-sqlcipher",
+                .upToNextMajor(from: "1.9.0")
+            ),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "610.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.9.1")),
         .package(url: "https://github.com/kishikawakatsumi/keychainaccess", .upToNextMajor(from: "4.2.2")),
         .package(url: "https://github.com/amethystsoft/KeyringAccess", .upToNextMajor(from: "1.0.0")),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Vein",
-            dependencies: veinDependencies
-        ),
-        .target(
-            name: "VeinCore",
-            dependencies: [
-                "Vein",
-                "VeinCoreMacros",
-            ]
-        ),
-        .target(
-            name: "VeinSwiftUI",
-            dependencies: [
-                "Vein",
-                "VeinSwiftUIMacros"
-            ]
-        ),
-        .target(
-            name: "VeinTesting",
-            dependencies: [
-                "Vein"
-            ]
-        ),
-        .macro(
-            name: "VeinCoreMacros",
-            dependencies: [
-                .product(name: "SwiftSyntax", package: "swift-syntax"),
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-                .product(name: "VeinMacrosCore", package: "VeinMacrosCore"),
-            ]
-        ),
-        .macro(
-            name: "VeinSwiftUIMacros",
-            dependencies: [
-                .product(name: "SwiftSyntax", package: "swift-syntax"),
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-                .product(name: "VeinMacrosCore", package: "VeinMacrosCore"),
-            ]
-        ),
-        .target(
-            name: "VeinMacrosBaseWrapper",
-            dependencies: [
-                .product(name: "VeinMacrosCore", package: "VeinMacrosCore")
-            ],
-        ),
-        .target(name: "ULID"),
-        .testTarget(
-            name: "VeinTests",
-            dependencies: [
-                "Vein",
-                "VeinCore",
-                .product(name: "Logging", package: "swift-log"),
-                .product(name: "SQLiteDB", package: "swift-sqlcipher"),
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
-            ]
-        ),
-        .testTarget(
-            name: "VeinTestingTests",
-            dependencies: [
-                "VeinTesting",
-                "Vein",
-                "VeinCore",
-                "VeinCoreMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "Logging", package: "swift-log")
-            ]
-        ),
-        .testTarget(
-            name: "ULIDTests",
-            dependencies: ["ULID"]
-        )
-    ]
+    targets: targets
 )
