a3285c6ac606d30895bdb8b8cf76c696bdfa94d6
Mia Koring <koring.mia@gmail.com>
2026-07-03 21:17:19 +0200

    added keyring information to tutorial

--- Sources/VeinCore/VeinCore.docc/Resources/quickstart-use-2.swift
+++ Sources/VeinCore/VeinCore.docc/Resources/quickstart-use-2.swift
@@ -1,6 +1,15 @@
 import VeinCore
 
 func setupAndUseVein() throws {
+    // When encryption is enabled and you're running on Linux,
+    // you need to tell KeyringAccess your appID too.
+    // If Linux is not one of your targets you can ignore it.
+#if os(Linux)
+    Keyring.appIdentifier.withLock { identifier in
+        identifier = "com.example.app"
+    }
+#endif
+    
     let container = ModelContainer(
         V0_0_1.self, // Your VersionedSchema
         migration: Migration.self, // Your SchemaMigrationPlan
