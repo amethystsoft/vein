83f357b33c413e8bcab9993891dcaf2fddd410fc
Mia Koring <koring.mia@gmail.com>
2026-07-07 21:53:09 +0200

    added swiftui tutorial and star button.

---
+++ Sources/VeinSwiftUI/VeinSwiftUI.docc/Resources/use-app-1.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import VeinSwiftUI
+
+@main
+struct VeinTestEnvironmentApp: App {
+    let modelContainer: ModelContainer
+    
+    init() {
+        // Initialize ModelContainer
+        self.modelContainer = initializedModelContainer
+    }
+    
+    var body: some Scene {
+        WindowGroup("VeinExample") {
+            ContentView()
+        }
+    }
+}
