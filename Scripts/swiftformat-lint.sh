829ea4deb894170307427309169bce74cb25bd4b
Mia Koring <koring.mia@gmail.com>
2026-07-20 19:20:42 +0200

    added swiftformat lint action

---
+++ Scripts/swiftformat-lint.sh
@@ -0,0 +1,7 @@
+cd "$(dirname "$0")"/../ || exit 1
+
+if [ -z "$1" ]; then
+  swiftformat . --exclude "Sources/ULID/*,Tests/ULIDTests/*,Sources/VeinCore/VeinCore.docc/*,Sources/VeinSwiftUI/VeinSwiftUI.docc/*" --lint
+else
+  swiftformat $1
+fi
