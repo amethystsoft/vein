e22d0ecec39e742162b9001a7c9a620384ae518b
Mia Koring <koring.mia@gmail.com>
2026-07-23 15:43:04 +0200

    formatting run

--- Tests/VeinTests/Windows/CredStoreTests.swift
+++ Tests/VeinTests/Windows/CredStoreTests.swift
@@ -1,24 +1,36 @@
-#if canImport(WinSDK)
-@testable import Vein
-import Testing
+// ===----------------------------------------------------------------------===
+//
+// This source file is part of the Amethyst Vein open source project
+//
+// Copyright (c) 2026 Mia Koring.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// ===----------------------------------------------------------------------===
 
-@Suite
-struct WinCredentialTest {
-	@Test
-	func ensureEncodeDecode() async throws {
-		let ressource = "de.amethystsoft.vein.WinCredentialTest"
-		let username = "VeinCredentialTest"
-		let secret = "test123-"
-		
-		WinCredential.store(
-			ressource: ressource,
-			username: username,
-			secret: secret
-		)
-		
-		let result = WinCredential.retrieve(ressource: ressource)
-		
-		#expect(secret == result)
-	}
-}
+#if canImport(WinSDK)
+    @testable import Vein
+    import Testing
+
+    @Suite
+    struct WinCredentialTest {
+        @Test
+        func ensureEncodeDecode() async throws {
+            let ressource = "de.amethystsoft.vein.WinCredentialTest"
+            let username = "VeinCredentialTest"
+            let secret = "test123-"
+
+            WinCredential.store(
+                ressource: ressource,
+                username: username,
+                secret: secret
+            )
+
+            let result = WinCredential.retrieve(ressource: ressource)
+
+            #expect(secret == result)
+        }
+    }
 #endif
