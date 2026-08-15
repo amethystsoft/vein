0843fa1f1fd6217e920719bd3bd3e75a0b426c09
Mia Koring <143010553+MiaKoring@users.noreply.github.com>
2026-08-15 02:10:59 +0200

    Add VeinSCUI to swiftformat lint exclusions

--- Scripts/swiftformat-lint.sh
+++ Scripts/swiftformat-lint.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"/../ || exit 1
 
 if [ -z "$1" ]; then
-  swiftformat . --exclude "Sources/ULID/*,Tests/ULIDTests/*,Sources/VeinCore/VeinCore.docc/*,Sources/VeinSwiftUI/VeinSwiftUI.docc/*" --lint
+  swiftformat . --exclude "Sources/ULID/*,Tests/ULIDTests/*,Sources/VeinCore/VeinCore.docc/*,Sources/VeinSwiftUI/VeinSwiftUI.docc/*,Sources/VeinSCUI/VeinSCUI.docc/*" --lint
 else
   swiftformat "$1" --lint
 fi
